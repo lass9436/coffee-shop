c89929d292c004c702132a1e521b89711c14ccfe
lass9436 <lass9436@gmail.com>
2023-11-20 22:34:43 +0900

    data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `user` (id, password, point) VALUES ('user1', 'password1', 100);
+INSERT INTO `user` (id, password, point) VALUES ('user2', 'password2', 150);
+
+INSERT INTO menu (name, price) VALUES ('Espresso', 3000);
+INSERT INTO menu (name, price) VALUES ('Cappuccino', 4500);
+INSERT INTO menu (name, price) VALUES ('Latte', 4000);
