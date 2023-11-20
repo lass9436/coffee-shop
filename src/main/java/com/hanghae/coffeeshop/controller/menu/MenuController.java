1bfdc6531ce8681b9ef39342442ba02dd394200c
lass9436 <lass9436@gmail.com>
2023-11-20 22:35:29 +0900

    get method 로 수정

--- src/main/java/com/hanghae/coffeeshop/controller/menu/MenuController.java
+++ src/main/java/com/hanghae/coffeeshop/controller/menu/MenuController.java
@@ -3,7 +3,7 @@ package com.hanghae.coffeeshop.controller.menu;
 import com.hanghae.coffeeshop.domain.menu.Menu;
 import com.hanghae.coffeeshop.service.menu.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +20,6 @@ public class MenuController {
         this.menuService = menuService;
     }
 
-    @PostMapping("/list")
+    @GetMapping("/list")
     public List<Menu> getMenuList() { return menuService.getMenuList(); }
 }
